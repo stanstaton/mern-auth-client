3f94782994a40b1106ed0eb60a83f05d6218d617
stanstaton <thatkidenzo@gmail.com>
2019-09-12 14:27:04 -0700

    added sign up form

--- src/Content/Pages/Signup.js
+++ src/Content/Pages/Signup.js
@@ -1,11 +1,51 @@
 import React from 'react'
+import axios from 'axios'
+import {Redirect} from 'react-router-dom'
 
-const Signup = props => {
-    return (
-        <div>
-            <h2>Sign-up</h2>
-        </div>
-    )
+class Signup extends React.Component {
+
+    state = {
+        firstname: '',
+        lastname: '',
+        email: '',
+        password: ''
+    }
+
+    storeInput = (e) => {
+        this.setState({[e.target.name]: e.target.value})
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault()
+        console.log('Submitted', this.state)
+    }
+
+    render(){ 
+        return (
+            <div>
+                <h2>Sign-up</h2>
+                <form onSubmit={this.handleSubmit}>
+                    <div>
+                        <label>First Name:</label>
+                        <input name="firstname" placeholder="Your First Name" onChange={this.storeInput} />
+                    </div>
+                    <div>
+                        <label>Last Name:</label>
+                        <input name="lastname" placeholder="Your Last Name" onChange={this.storeInput} />
+                    </div>
+                    <div>
+                        <label>Email:</label>
+                        <input name="email" placeholder="Your E-mail Address" onChange={this.storeInput} />
+                    </div>
+                    <div>
+                        <label>Password:</label>
+                        <input type="password" name="password" placeholder="Your Password" onChange={this.storeInput} />
+                    </div>
+                    <button type="submit" >Sign Me Up!</button>
+                </form>
+            </div>
+        )
+    }
 }
 
 export default Signup
